e54daa7f1484cc5ada86eff235d9bb030bd25880
Jürgen Huber <jue.huber@gmail.com>
2019-04-25 18:02:24 +0200

    initial commit

---
+++ 2015_GA1_Aufgabe2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2015_GA1_Aufgabe2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
